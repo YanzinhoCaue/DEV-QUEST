9d7a88fe589ba3f94f3dbbf23286048fb9522418
Yan Caue <coldecmastercrelan@gmail.com>
2023-04-11 10:34:09 -0300

    arquivo adicionado

---
+++ MODULO JAVASCRIPT MEDIO/ESCOPO/src/js/index.js
@@ -0,0 +1,32 @@
+// if(true){
+//     const mensagem = 'Olá'
+// }
+// console.log(mensagem)
+
+// {
+//     var mensagem = 'Olá'
+
+// }
+// console.log(mensagem)
+
+// function falar(){
+//     var mensagem = 'Ola Mundo'
+//     console.log(mensagem)
+// }
+// falar()
+// console.log(mensagem)
+
+
+// function falar(){
+//     var mensagem = 'Ola Mundo'
+//     console.log(mensagem)
+//     function falarOutraCoisa(){
+        
+//     }
+//     falarOutraCoisa()
+//     console.log(falarOutraCoisa)
+// }
+// falar()
+// console.log(mensagem)
+// console.log(falarOutraCoisa)
+console.log(url)
---
+++ MODULO JAVASCRIPT MEDIO/HOISTING/src/js/index.js
@@ -0,0 +1,5 @@
+// var pai // declaração
+// pais = 'Brasil' // atribuição
+let pais
+console.log(pais)
+pais = 'Brasil'
