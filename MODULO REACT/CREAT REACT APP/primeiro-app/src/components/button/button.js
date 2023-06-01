04c71fbec4cf8f0174932685ce7d47f2b550cada
Yan Caue <coldecmastercrelan@gmail.com>
2023-06-01 08:51:05 -0300

    novos arquivos

---
+++ MODULO REACT/CREAT REACT APP/primeiro-app/src/components/button/button.js
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>Clique aqui</button>
+}
+
+export default Button
---
+++ MODULO REACT/CREAT REACT APP/primeiro-app/src/components/card/card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Card extends React.Component {
+  render(){
+    return (
+      <div className='card'>
+        <h3>Titulo</h3>
+        <p>esse é um texto do card</p>
+      </div>
+    )
+  }
+}
+
+export default Card
